86029e8b33ced8b11f711bbe75e83d4dc71b3f53
unknown <jezrael88@gmail.com>
2021-07-21 18:29:12 +0800

    added slier for partners

--- themes/educenter-hugo-master/assets/js/script.js
+++ themes/educenter-hugo-master/assets/js/script.js
@@ -63,6 +63,45 @@
     });
   });
 
+  // clients logo slider
+	$('.client-logo-slider').slick({
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		autoplay: true,
+		dots: false,
+		arrows: false,
+		responsive: [{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 400,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+
   //  Count Up
   function counter() {
     var oTop;
